cda3fbb172d2a589c07e32d7fd736d5107ce012e
dune98 <diomedi79@gmail.com>
2020-04-07 16:47:43 +0200

    new backend

---
+++ justmeet_backend/src/main/kotlin/com/diegodiome/justmeet_backend/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.diegodiome.justmeet_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
